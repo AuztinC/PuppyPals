2566c5ca4476c80407f73bc99505b3e6760fa791
Austin Cripe <aaustinc@live.com>
2023-09-05 15:17:40 -0400

    finale

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { puppyList } from "./data";
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find(pup => pup.id === featPupId)
+  // console.log(puppies)
   return (
     <>
-
+      { featPupId != null ? <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div> : null
+      }
+      {puppies.map(pup => {
+        console.log(featuredPup)
+        return <p key={pup.id} onClick={()=>{setFeatPupId(pup.id)}}>{pup.name}</p>
+      })}
     </>
   )
 }
